00ec37393a3bc47bd45d1b35782b46156c21c726
Fritz Elfert <fritz@fritz-elfert.de>
2025-05-07 22:49:26 +0200

    WIP

--- src/main/java/jenkins/plugins/jclouds/cli/JCloudsCreateUserdataCommand.java
+++ src/main/java/jenkins/plugins/jclouds/cli/JCloudsCreateUserdataCommand.java
@@ -19,6 +19,7 @@ import com.thoughtworks.xstream.XStreamException;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 import hudson.Extension;
@@ -27,6 +28,13 @@ import hudson.cli.CLICommand;
 import jenkins.model.Jenkins;
 
 import jenkins.plugins.jclouds.config.ConfigExport;
+import jenkins.plugins.jclouds.config.UserDataBoothook;
+import jenkins.plugins.jclouds.config.UserDataInclude;
+import jenkins.plugins.jclouds.config.UserDataIncludeOnce;
+import jenkins.plugins.jclouds.config.UserDataPartHandler;
+import jenkins.plugins.jclouds.config.UserDataScript;
+import jenkins.plugins.jclouds.config.UserDataUpstart;
+import jenkins.plugins.jclouds.config.UserDataYaml;
 import jenkins.plugins.jclouds.internal.CryptoHelper;
 
 import org.kohsuke.args4j.Argument;
@@ -46,15 +54,18 @@ import org.jenkinsci.lib.configprovider.model.Config;
 @Extension
 public class JCloudsCreateUserdataCommand extends CLICommand {
 
-    @Option(name = "--overwrite", usage = "Overwrite existing userdata files")
+    @Option(name = "--overwrite", forbids={"--merge"}, usage = "Overwrite existing userdata files")
     private boolean overwrite;
+
+    @Option(name = "--merge", forbids={"--overwrite"}, usage = "Generate new Ids for all imported userdata files")
+    private boolean merge;
 
     @Argument(required = false, metaVar = "CREDENTIAL", usage = "ID of credential (RSA key) to encrypt data. Default: Taken from input XML")
     private String cred = null;
 
     @Override
     public String getShortDescription() {
-        return Messages.GetUserdataCommand_shortDescription();
+        return Messages.CreateUserdataCommand_shortDescription();
     }
 
     @Override
@@ -80,6 +91,40 @@ public class JCloudsCreateUserdataCommand extends CLICommand {
             } catch (XStreamException e) {
                 throw new IllegalStateException("Unable to parse input: " + e.toString());
             }
+        }
+
+        if (merge) {
+            stdout.println("<replacements>");
+            List<Config> newcfgs = new ArrayList<>();
+            for (Config cfg : cfgs) {
+                Config ncfg = null;
+                if (cfg instanceof UserDataBoothook) {
+                    ncfg = ((UserDataBoothook)cfg).dup();
+                }
+                if (cfg instanceof UserDataInclude) {
+                    ncfg = ((UserDataInclude)cfg).dup();
+                }
+                if (cfg instanceof UserDataIncludeOnce) {
+                    ncfg = ((UserDataIncludeOnce)cfg).dup();
+                }
+                if (cfg instanceof UserDataPartHandler) {
+                    ncfg = ((UserDataPartHandler)cfg).dup();
+                }
+                if (cfg instanceof UserDataScript) {
+                    ncfg = ((UserDataScript)cfg).dup();
+                }
+                if (cfg instanceof UserDataUpstart) {
+                    ncfg = ((UserDataUpstart)cfg).dup();
+                }
+                if (cfg instanceof UserDataYaml) {
+                    ncfg = ((UserDataYaml)cfg).dup();
+                }
+                stdout.println(String.format("  <replacement old=\"%s\" new=\"%s\"/>",
+                          cfg.id, ncfg.id));
+                newcfgs.add(ncfg);
+            }
+            stdout.println("</replacements>");
+            cfgs = newcfgs;
         }
 
         if (!overwrite) {
